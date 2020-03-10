e2cf23da2fc67434daa5dce5b786da984f6eb6cf
M. Fadli Zein <m.fadli.zein.17@gmail.com>
2020-03-10 13:40:29 +0700

    suksess

--- app/src/main/java/com/gzeinnumer/scheduletasksworkmanager/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/scheduletasksworkmanager/MainActivity.java
@@ -3,9 +3,11 @@ package com.gzeinnumer.scheduletasksworkmanager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.work.Constraints;
 import androidx.work.OneTimeWorkRequest;
+import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import java.util.concurrent.TimeUnit;
 
@@ -16,29 +18,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //ssat activity beralan, lasung access ini
-//        OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class)
-//                .addTag("uploadWorkRequest").build();
-//        WorkManager.getInstance(getApplicationContext()).enqueue(uploadWorkRequest);
+        OneTimeWorkRequest uploadWorkRequest = new OneTimeWorkRequest.Builder(UploadWorker.class)
+                .addTag("uploadWorkRequest").build();
+        WorkManager.getInstance(getApplicationContext()).enqueue(uploadWorkRequest);
 
         //jalankan jika hp di cass maka ini akan dijalankan
-//        Constraints constraints = new Constraints.Builder()
-//                .setRequiresDeviceIdle(true)
-//                .setRequiresCharging(true)
-//                .build();
-//        OneTimeWorkRequest compressionWork =
-//                new OneTimeWorkRequest.Builder(CompressWorker.class)
-//                        .setConstraints(constraints)
-//                        .addTag("compressionWork")
-//                        .build();
-//        WorkManager.getInstance(getApplicationContext()).enqueue(compressionWork);
+        Constraints constraints = new Constraints.Builder()
+                .setRequiresDeviceIdle(true)
+                .setRequiresCharging(true)
+                .build();
+        OneTimeWorkRequest compressionWork =
+                new OneTimeWorkRequest.Builder(CompressWorker.class)
+                        .setConstraints(constraints)
+                        .addTag("compressionWork")
+                        .build();
+        WorkManager.getInstance(getApplicationContext()).enqueue(compressionWork);
 
         //jalankan jika setiap 15 menit
-        OneTimeWorkRequest onTime = new OneTimeWorkRequest.Builder(TimeWorker.class)
-                .setInitialDelay(15, TimeUnit.MINUTES)
-                .addTag("onTime")
-                .build();
-
-        WorkManager.getInstance(getApplicationContext()).enqueue(onTime);
+        Log.d("MYZEIN", "jalan kok");
+        PeriodicWorkRequest workRequest = new PeriodicWorkRequest.Builder(TimeWorker.class, 15, TimeUnit.MINUTES).build();
+        WorkManager.getInstance(getApplicationContext()).enqueue(workRequest);
 
     }
 }
